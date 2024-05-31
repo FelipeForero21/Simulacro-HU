9a39441ef0985b7ebc332863fb162605bacb4454
FelipeForero21 <elipeforero21@gmail.com>
2024-05-31 11:55:26 -0500

    Add validation between 6 p.m. m. and 6 a.m. m. to place orders

--- src/sells/sells.controller.ts
+++ src/sells/sells.controller.ts
@@ -6,25 +6,42 @@ import {
   Patch,
   Param,
   Delete,
+  ParseIntPipe,
+  Query,
+  HttpException,
+  HttpStatus,
+  ForbiddenException,
+  BadRequestException,
 } from '@nestjs/common';
 import { SellsService } from './sells.service';
 import { CreateSellDto } from './dto/create-sell.dto';
 import { UpdateSellDto } from './dto/update-sell.dto';
 import { ApiOperation, ApiParam, ApiBody, ApiTags } from '@nestjs/swagger';
+import { Sell } from './entities/sell.entity';
 
 @Controller('sells')
 @ApiTags('Sells')
 export class SellsController {
   constructor(private readonly sellsService: SellsService) {}
 
-  @ApiOperation({ summary: 'Create a new sell' }) 
+  @ApiOperation({ summary: 'Create a new sell' })
   @ApiBody({ type: CreateSellDto })
   @Post('/newSell')
   create(@Body() createSellDto: CreateSellDto) {
-    return this.sellsService.create(createSellDto);
+    try {
+      return this.sellsService.create(createSellDto);
+    } catch (error) {
+      if (error instanceof ForbiddenException) {
+        throw new HttpException(
+          'Orders cannot be processed between 6pm and 6am',
+          HttpStatus.FORBIDDEN,
+        );
+      }
+      throw error;
+    }
   }
 
-  @ApiOperation({ summary: 'Get all sells' }) 
+  @ApiOperation({ summary: 'Get all sells' })
   @Get('/all')
   findAll() {
     return this.sellsService.findAll();
@@ -36,4 +53,5 @@ export class SellsController {
   remove(@Param('id') id: string) {
     return this.sellsService.remove(+id);
   }
+
 }
